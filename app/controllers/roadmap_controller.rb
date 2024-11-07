3e2f7f38d3a11899778d956bbbb294957a16454b
Hieu Chu <minhieuchu@gmail.com>
2024-11-07 19:57:12 +0700

    Add API to update roadmap

--- app/controllers/roadmap_controller.rb
+++ app/controllers/roadmap_controller.rb
@@ -4,20 +4,14 @@ class RoadmapController < ApplicationController
   end
 
   def create
-    if params[:step_ids] # Todo: validate array type before controller action
+    if params[:step_ids] # Todo: validate before controller action
       roadmap_id, *step_ids = params[:step_ids]
-      mongodb_update_array_filters = []
-      mongodb_update_field_path = "steps"
-
-      step_ids.each_with_index do |step_id, index|
-        mongodb_update_array_filters << { "level#{index}._id" => BSON::ObjectId(step_id) }
-        mongodb_update_field_path = "#{mongodb_update_field_path}.$[level#{index}].steps"
-      end
+      array_filters, field_path = get_mongodb_update_params(step_ids).values_at(:array_filters, :push_field_path)
 
       resp = Roadmap.collection.find_one_and_update(
         { "_id" => BSON::ObjectId(roadmap_id) },
-        { "$push" => { mongodb_update_field_path => { _id: BSON::ObjectId.new, target: params[:target], steps: [] } } },
-        array_filters: mongodb_update_array_filters,
+        { "$push" => { field_path => { _id: BSON::ObjectId.new, target: params[:target], steps: [] } } },
+        array_filters: array_filters,
         return_document: :after,
       )
       render json: resp.to_json, status: :ok
@@ -27,5 +21,40 @@ class RoadmapController < ApplicationController
     target = params[:target]
     resp = Roadmap.create(target: target, steps: [])
     render json: resp.to_json, status: :ok
+  end
+
+  def update
+    roadmap_id, *step_ids = params[:step_ids]
+    array_filters, field_path = get_mongodb_update_params(step_ids).values_at(:array_filters, :set_field_path)
+
+    resp = Roadmap.collection.find_one_and_update(
+      { "_id" => BSON::ObjectId(roadmap_id) },
+      { "$set" => { field_path => params[:target] } },
+      array_filters: array_filters,
+      return_document: :after,
+    )
+    render json: resp.to_json, status: :ok
+  end
+
+  private
+
+  def get_mongodb_update_params(step_ids)
+    array_filters = []
+    push_field_path = "steps"
+    set_field_path = ""
+
+    step_ids.each_with_index do |step_id, index|
+      array_filters << { "level#{index}._id" => BSON::ObjectId(step_id) }
+      push_field_path = "#{push_field_path}.$[level#{index}].steps"
+      set_field_path = "#{set_field_path}steps.$[level#{index}]."
+    end
+
+    set_field_path = "#{set_field_path}target"
+
+    {
+      array_filters: array_filters,
+      push_field_path: push_field_path,
+      set_field_path: set_field_path,
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "roadmaps", to: "roadmap#index"
   post "create", to: "roadmap#create"
+  patch "update", to: "roadmap#update"
 end
