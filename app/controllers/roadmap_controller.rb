eefbed04bd626999acb7ff6d2caa3f7296159d6b
Hieu Chu <minhieuchu@gmail.com>
2024-11-08 16:06:04 +0700

    Add API to delete roadmap / step

--- app/controllers/roadmap_controller.rb
+++ app/controllers/roadmap_controller.rb
@@ -6,7 +6,8 @@ class RoadmapController < ApplicationController
   def create
     if params[:step_ids] # Todo: validate before controller action
       roadmap_id, *step_ids = params[:step_ids]
-      array_filters, field_path = get_mongodb_update_params(step_ids).values_at(:array_filters, :push_field_path)
+      array_filters, field_path =
+        get_mongodb_update_params(step_ids).values_at(:array_filters, :push_or_pull_field_path)
 
       resp = Roadmap.collection.find_one_and_update(
         { "_id" => BSON::ObjectId(roadmap_id) },
@@ -25,7 +26,8 @@ class RoadmapController < ApplicationController
 
   def update
     roadmap_id, *step_ids = params[:step_ids]
-    array_filters, field_path = get_mongodb_update_params(step_ids).values_at(:array_filters, :set_field_path)
+    array_filters, field_path =
+      get_mongodb_update_params(step_ids).values_at(:array_filters, :set_field_path)
 
     resp = Roadmap.collection.find_one_and_update(
       { "_id" => BSON::ObjectId(roadmap_id) },
@@ -36,25 +38,65 @@ class RoadmapController < ApplicationController
     render json: resp.to_json, status: :ok
   end
 
+  def delete
+    roadmap_id, *remaining_step_ids, pulled_step_id = params[:step_ids]
+    roadmap = Roadmap.find(roadmap_id)
+
+    if pulled_step_id.nil?
+      resp = roadmap.delete
+      render json: resp.to_json, status: :ok
+      return
+    end
+
+    array_filters, field_path =
+      get_mongodb_update_params(remaining_step_ids).values_at(:array_filters, :push_or_pull_field_path)
+
+    parent_step = roadmap
+    remaining_step_ids.each do |step_id|
+      next_step = parent_step.steps.find { |step| step._id == BSON::ObjectId(step_id) }
+      parent_step = next_step
+    end
+
+    pulled_step = parent_step.steps.find { |step| step._id == BSON::ObjectId(pulled_step_id) }
+
+    if params[:delete_recursive].nil? || params[:delete_recursive] == false
+      Roadmap.collection.find_one_and_update(
+        { "_id" => BSON::ObjectId(roadmap_id) },
+        { "$push" => { field_path => { "$each" => pulled_step.steps.as_json } } },
+        array_filters: array_filters,
+        return_document: :after,
+      )
+    end
+
+    resp = Roadmap.collection.find_one_and_update(
+      { "_id" => BSON::ObjectId(roadmap_id) },
+      { "$pull" => { field_path => { "_id" => BSON::ObjectId(pulled_step_id) } } },
+      array_filters: array_filters,
+      return_document: :after,
+    )
+
+    render json: resp.to_json, status: :ok
+  end
+
   private
 
   def get_mongodb_update_params(step_ids)
     array_filters = []
-    push_field_path = "steps"
     set_field_path = ""
+    push_or_pull_field_path = "steps"
 
     step_ids.each_with_index do |step_id, index|
       array_filters << { "level#{index}._id" => BSON::ObjectId(step_id) }
-      push_field_path = "#{push_field_path}.$[level#{index}].steps"
       set_field_path = "#{set_field_path}steps.$[level#{index}]."
+      push_or_pull_field_path = "#{push_or_pull_field_path}.$[level#{index}].steps"
     end
 
     set_field_path = "#{set_field_path}target"
 
     {
       array_filters: array_filters,
-      push_field_path: push_field_path,
       set_field_path: set_field_path,
+      push_or_pull_field_path: push_or_pull_field_path,
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "roadmaps", to: "roadmap#index"
   post "create", to: "roadmap#create"
   patch "update", to: "roadmap#update"
+  delete "delete", to: "roadmap#delete"
 end
